de4205dab2e08453fb099677a80157d32f619c4b
Emilgardis <Emilgardis@users.noreply.github.com>
2023-01-23 10:21:37 +0000

    deploy: 91736e6ebdab7a8c0da1b05a7e87b247c9d4bf2c

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"ember_mug":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"ember_mug/enum.KnownCharacteristic.html\" title=\"enum ember_mug::KnownCharacteristic\">KnownCharacteristic</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
