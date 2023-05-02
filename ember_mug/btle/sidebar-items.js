23d2f7452438ec49b6b5c2230cc635f61fc0a1a0
Emilgardis <Emilgardis@users.noreply.github.com>
2023-05-02 09:42:10 +0000

    deploy: 7676ec8daee353b75d11793b7b7cbd1dd66cd2d1

--- ember_mug/btle/sidebar-items.js
+++ ember_mug/btle/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["get_mugs","Get mugs on all adapters"],["get_mugs_on_adapters","Search for mugs on all adapters"],["search_adapter_for_ember","Search given adapter for a mug"]]};
+window.SIDEBAR_ITEMS = {"fn":["get_mugs","get_mugs_on_adapters","search_adapter_for_ember"]};
