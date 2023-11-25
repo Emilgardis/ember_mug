e71a68906a98b2a94864c8147a44f0b3d8e5de19
Emilgardis <Emilgardis@users.noreply.github.com>
2023-11-25 18:09:05 +0000

    deploy: aac108c8b4326fe7f969b794103b26fb02c2234c

--- implementors/core/cmp/trait.PartialEq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"ember_mug":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"enum\" href=\"ember_mug/enum.KnownCharacteristic.html\" title=\"enum ember_mug::KnownCharacteristic\">KnownCharacteristic</a>&gt; for <a class=\"enum\" href=\"ember_mug/enum.KnownCharacteristic.html\" title=\"enum ember_mug::KnownCharacteristic\">KnownCharacteristic</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"ember_mug":["",[["mug",[],["battery.rs","current_temp.rs","dsk.rs","last_location.rs","liquid_level.rs","liquid_state.rs","mug_color.rs","mug_meta.rs","name.rs","ota.rs","push_events.rs","target_temp.rs","temperature_unit.rs","time_date_zone.rs"]]],["btle.rs","lib.rs","mug.rs"]]\
-}');
-createSourceSidebar();
